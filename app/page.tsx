f991326f28a92c67075f194244390d641c34d09e
Libby's Birthday Site <libby@example.com>
2026-01-15 10:43:39 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,13 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { Envelope } from "@/components/Envelope";
 import { Invitation } from "@/components/Invitation";
-import { MusicPlayer } from "@/components/MusicPlayer";
 
 export default function Home() {
   const [isEnvelopeOpen, setIsEnvelopeOpen] = useState(false);
 
-  // Eden - Happy Birthday (Eurovision version - 100% embeddable)
-  const youtubeLink = "https://www.youtube.com/watch?v=wz-kLp6Ww7k";
-
   return (
     <main className="min-h-screen bg-cream-50 overflow-x-hidden flex flex-col">
       <Header />
-
-      <MusicPlayer isPlaying={isEnvelopeOpen} videoUrl={youtubeLink} />
 
       <div className="flex-1 relative">
         {!isEnvelopeOpen ? (
